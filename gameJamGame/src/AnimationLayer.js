7e27012846e7cbe3cdfbd8e15272f69e990c496c
Pierre-Morgan Gate <pierremorgan.gate@gmail.com>
2014-11-01 16:02:17 -0700

    Start physics chipmunk

--- gameJamGame/src/AnimationLayer.js
+++ gameJamGame/src/AnimationLayer.js
@@ -1,6 +1,8 @@
 var AnimationLayer = cc.Layer.extend({
-    ctor:function () {
+    space:null,
+    ctor:function (space) {
         this._super();
+        this.space = space;
         this.init();
     },
     init:function () {
@@ -9,19 +11,28 @@ var AnimationLayer = cc.Layer.extend({
         var winsize = cc.director.getWinSize();
         var centerPos = cc.p(winsize.width / 2, winsize.height / 2);
 
-        var player = new cc.Sprite(res.Player_png);
-        player.attr({x: 80, y: 85});
+        // Let's add the player
+        this.player = new cc.PhysicsSprite.create(res.Player_png);
+        var playerSize = this.player.getContentSize();
+        this.playerBody = new cp.Body(1, cp.momentForBox(1, playerSize.width, playerSize.height));
+        this.playerBody.setPos(cc.p(winsize.width / 2, winsize.height / 2 + 48))
+        this.space.addBody(this.playerBody);
+        this.playerShape = new cp.BoxShape(this.playerBody, playerSize.width, playerSize.height);
+        this.space.addShape(this.playerShape);
+        this.player.setBody(this.playerBody);
 
-        var elevator = new cc.Sprite(res.Elevator_png);
-        elevator.setPosition(centerPos)
+        // Let's add the elevator
+        this.elevator = new cc.PhysicsSprite.create(res.Elevator_png);
+        var elevatorSize = this.elevator.getContentSize();
+        this.elevatorBody = new cp.Body(1, cp.momentForBox(1, elevatorSize.width, elevatorSize.height));
+        this.elevatorBody.setPos(cc.p(winsize.width / 2, winsize.height / 2 - 48))
+        this.space.addBody(this.elevatorBody);
+        this.elevatorShape = new cp.BoxShape(this.elevatorBody, elevatorSize.width, elevatorSize.height);
+        this.space.addShape(this.elevatorShape);
+        this.elevator.setBody(this.elevatorBody);
 
-        //var actionTo = new cc.MoveTo(2, cc.p(300, 85));
-        //player.runAction(new cc.Sequence(actionTo));
-
-        this.addChild(player);
-        this.addChild(elevator);
-
-
+        this.addChild(this.player);
+        this.addChild(this.elevator);
 
     }
 });
--- gameJamGame/src/MainScene.js
+++ gameJamGame/src/MainScene.js
@@ -1,8 +1,21 @@
 var MainScene = cc.Scene.extend({
+    space:null,
+    initPhysics:function() {
+        this.space = new cp.Space();
+        this.space.gravity = cp.v(0, -10);
+        var size = cc.director.getWinSize();
+        var floor = new cp.SegmentShape(this.space.staticBody, cp.v(0, 0), cp.v(size.width, 0), 0)
+        this.space.addStaticShape(floor);
+    },
     onEnter:function () {
         this._super();
+        this.initPhysics();
         this.addChild(new BackgroundLayer());
-        this.addChild(new AnimationLayer());
+        this.addChild(new AnimationLayer(this.space));
         this.addChild(new ScoreLayer());
+        this.scheduleUpdate();
+    },
+    update:function (dt) {
+        this.space.step(dt);
     }
 });
