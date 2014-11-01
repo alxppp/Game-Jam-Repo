20043d8d6dd0a0b16c00b646827458648f0c4002
Alexander Popp <alxppp@googlemail.com>
2014-11-01 16:58:50 -0700

    Only x

--- gameJamGame/src/AnimationLayer.js
+++ gameJamGame/src/AnimationLayer.js
@@ -34,40 +34,19 @@ var AnimationLayer = cc.Layer.extend({
         this.addChild(this.player);
         this.addChild(this.elevator);
 
+        player = this.player;
+
         var touchListener = cc.EventListener.create({
             event: cc.EventListener.TOUCH_ONE_BY_ONE,
             swallowTouches: true,
             onTouchBegan: function (touch, event) {
-                /*var target = event.getCurrentTarget();
-
-                var locationInNode = target.convertToNodeSpace(touch.getLocation());
-                var s = target.getContentSize();
-                var rect = cc.rect(0, 0, s.width, s.height);
-
-                if (cc.rectContainsPoint(rect, locationInNode)) {
-                    cc.log("sprite began... x = " + locationInNode.x + ", y = " + locationInNode.y);
-                    target.opacity = 180;
-                    return true;
-                }*/
-                return false;
+                return true;
             },
             onTouchMoved: function (touch, event) {
-                /*var target = event.getCurrentTarget();
+                var target = player;
                 var delta = touch.getDelta();
                 target.x += delta.x;
-                target.y += delta.y;*/
-                alert('hi');
-            }/*,
-            onTouchEnded: function (touch, event) {
-                var target = event.getCurrentTarget();
-                cc.log("sprite onTouchesEnded.. ");
-                target.setOpacity(255);
-                if (target == sprite2) {
-                    sprite1.setLocalZOrder(100);
-                } else if (target == sprite1) {
-                    sprite1.setLocalZOrder(0);
-                }
-            }*/
+            }
         });
 
         cc.eventManager.addListener(touchListener, this);
